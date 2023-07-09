909bf66b9db6d6e368cb08369b0b25430611668d
Slowhand0309 <slowhand0309@gmail.com>
2023-07-10 07:12:08 +0900

    :construction: 正規表現組み込み

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{env, fs::File, io::Read};
 
+use regex::Regex;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
@@ -8,5 +10,14 @@ fn main() {
     let mut f = File::open(filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
-    println!("text: {}", contents);
+    // println!("text: {}", contents);
+
+    let mut images: Vec<(String, String)> = Vec::new();
+    let re = Regex::new(r"!\[(.*?)\]\((.*?)\)").unwrap();
+    for caps in re.captures_iter(&contents) {
+        // println!("Title: {}", &caps[1]);
+        // println!("Url: {}", &caps[2]);
+        images.push((String::from(&caps[1]), String::from(&caps[2])));
+    }
+    println!("images {:?}", images);
 }
