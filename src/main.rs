0935388fbc2dcfc697dc69cee90d43502cdcfa86
Slowhand0309 <slowhand0309@gmail.com>
2023-07-21 06:46:25 +0900

    :construction: リソースのコピーとリネーム実装

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs::File, fs::copy, io::Read, path::Path};
+use std::{env, fs::File, fs::copy, fs::create_dir_all, io::Read, path::Path};
 
 use percent_encoding::percent_decode_str;
 use regex::Regex;
@@ -25,48 +25,47 @@ struct Resource {
     new: Meta,
 }
 
-fn rename_and_move(resources: &Vec<Resource>) {
+fn rename_and_move(resources: &mut Vec<Resource>, article_id: &String) {
+    let mut i = 1;
     for r in resources {
-        println!("resource {:?}", r);
-
         // デコードして指定されてディレクトリへリネーム&コピー
         let path = Path::new(&r.old.url);
         let decoded = percent_decode_str(path.to_str().unwrap()).decode_utf8_lossy();
-        // println!("decoded {:?}", decoded);
-        // let exists = Path::new("res").join(&decoded.to_string()).exists();
-        // println!("exists {:?}", exists);
 
-        // TODO: コピー先のディレクトリ作成
-        let result = copy(Path::new("res").join(&decoded.to_string()).to_str().unwrap(), "res/test.png");
-        println!("result {:?}", result);
-        // let path = Path::new(&r.old.url).parent();
-        // if let Some(path) = path {
-        //     let decoded = percent_decode_str(path.to_str().unwrap()).decode_utf8_lossy();
-        //     println!("decoded {:?}", decoded);
-        //     Path::new("/bin").exists()
-        // }
+        // コピー先のディレクトリ作成
+        create_dir_all(format!("res/{article_id}")).expect("create dir failed");
+        let ext = Path::extension(&path)
+                            .expect("get extension failed")
+                            .to_str()
+                            .unwrap_or("png");
+        copy(Path::new("res")
+                    .join(&decoded.to_string())
+                    .to_str()
+                    .unwrap(), format!("res/{article_id}/image{i}.{ext}")).expect("copy failed");
+
+        r.new.title = r.old.title.clone();
+        r.new.url = format!("res/{article_id}/image{i}.{ext}");
+        i += 1;
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
-    println!("filename {}", filename);
+    let article_id = &args[2];
+    println!("filename {}, article_id {}", filename, article_id);
 
     let mut f = File::open(filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
-    // println!("text: {}", contents);
 
     let mut resources: Vec<Resource> = Vec::new();
     let re = Regex::new(r"!\[(.*?)\]\((.*?)\)").unwrap();
     for caps in re.captures_iter(&contents) {
-        // println!("Title: {}", &caps[1]);
-        // println!("Url: {}", &caps[2]);
         resources.push(Resource { old: Meta::from(&caps[1], &caps[2]), new: Meta::empty() });
     }
 
     // リソースを移動しリネームする
-    rename_and_move(&resources);
-
+    rename_and_move(&mut resources, &article_id);
+    println!("resources: {:?}", resources);
 }
